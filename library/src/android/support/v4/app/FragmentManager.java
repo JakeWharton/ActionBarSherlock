5969759fd4a59d201d00ddaa17239542d6316a17
Jake Wharton <jakewharton@gmail.com>
2011-07-08 08:38:54 -0400

    Port new style of activity atachment from `native-backport` branch. Closes #35.
    Conflicts:
    
    	library/src/android/support/v4/app/FragmentActivity.java
    	library/src/com/actionbarsherlock/internal/app/ActionBarSupportImpl.java

--- library/src/android/support/v4/app/FragmentManager.java
+++ library/src/android/support/v4/app/FragmentManager.java
@@ -1256,6 +1256,7 @@ final class FragmentManagerImpl extends FragmentManager {
             if (mActivity == null) {
                 throw new IllegalStateException("Activity has been destroyed");
             }
+            mActivity.ensureActionBarAttached();
             if (mPendingActions == null) {
                 mPendingActions = new ArrayList<Runnable>();
             }
