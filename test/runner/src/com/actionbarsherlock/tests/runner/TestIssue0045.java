3305d68165d1e3715da6798aa2d7fc84ad351c44
Jake Wharton <jakewharton@gmail.com>
2011-07-29 11:44:11 -0400

    Add test case for the type returned by getSubMenu() on a menu item. Refs #45.

---
+++ test/runner/src/com/actionbarsherlock/tests/runner/TestIssue0045.java
@@ -0,0 +1,17 @@
+package com.actionbarsherlock.tests.runner;
+
+import android.test.suitebuilder.annotation.Smoke;
+import com.actionbarsherlock.tests.app.Issue0045;
+
+public class TestIssue0045 extends BaseTestCase<Issue0045> {
+	public TestIssue0045() {
+		super(Issue0045.class);
+	}
+	
+	@Smoke
+	public void testMenuFindItemDoesNotError() throws InterruptedException {
+		Object menuItem = getActivity().getMenuItemSubMenu();
+		assertNotNull(menuItem);
+		assertTrue(menuItem instanceof android.support.v4.view.SubMenu);
+	}
+}
