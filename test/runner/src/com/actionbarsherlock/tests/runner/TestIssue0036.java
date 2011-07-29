88c067ce4716c91a872454e20c033cb884c4f690
Jake Wharton <jakewharton@gmail.com>
2011-07-29 11:15:20 -0400

    Test a sub-menus parent is of the proper instance. Refs #36.

---
+++ test/runner/src/com/actionbarsherlock/tests/runner/TestIssue0036.java
@@ -0,0 +1,17 @@
+package com.actionbarsherlock.tests.runner;
+
+import android.test.suitebuilder.annotation.Smoke;
+import com.actionbarsherlock.tests.app.Issue0036;
+
+public class TestIssue0036 extends BaseTestCase<Issue0036> {
+	public TestIssue0036() {
+		super(Issue0036.class);
+	}
+	
+	@Smoke
+	public void testMenuFindItemDoesNotError() throws InterruptedException {
+		Object menuItem = getActivity().getSubMenuItemParent();
+		assertNotNull(menuItem);
+		assertTrue(menuItem instanceof android.support.v4.view.MenuItem);
+	}
+}
