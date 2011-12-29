db568fd539923a434ea5307f2eddc8ee5b870868
JakeWharton <jakewharton@gmail.com>
2011-12-29 10:52:44 -0500

    Add collapsed tab navigation sample. Closes #142.

---
+++ samples/demos/src/com/actionbarsherlock/sample/demos/TabNavigationCollapsed.java
@@ -0,0 +1,42 @@
+package com.actionbarsherlock.sample.demos;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.sample.demos.R;
+
+public class TabNavigationCollapsed extends SherlockActivity implements ActionBar.TabListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text);
+
+        //The following two options trigger the collapsing of the main action bar view.
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        for (int i = 0; i < 3; i++) {
+            ActionBar.Tab tab = getSupportActionBar().newTab();
+            tab.setText("Tab " + i);
+            tab.setTabListener(this);
+            getSupportActionBar().addTab(tab);
+        }
+    }
+
+    @Override
+    public void onTabReselected(Tab tab) {
+    }
+
+    @Override
+    public void onTabSelected(Tab tab) {
+        ((TextView)findViewById(R.id.text)).setText("Selected: " + tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab) {
+    }
+}
