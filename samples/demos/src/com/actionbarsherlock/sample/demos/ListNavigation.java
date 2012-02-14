04e3847db072408f4e12260f4b9bf167961026f3
Jake Wharton <jakewharton@gmail.com>
2012-02-14 14:50:59 -0500

    Add content text to list navigation.

--- samples/demos/src/com/actionbarsherlock/sample/demos/ListNavigation.java
+++ samples/demos/src/com/actionbarsherlock/sample/demos/ListNavigation.java
@@ -8,24 +8,31 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class ListNavigation extends SherlockActivity implements ActionBar.OnNavigationListener {
+    private TextView mSelected;
+    private String[] mLocations;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         setTheme(SampleList.THEME); //Used for theme switching in samples
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.text);
 
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        setContentView(R.layout.list_navigation);
+        mSelected = (TextView)findViewById(R.id.text);
+
+        mLocations = getResources().getStringArray(R.array.locations);
 
         //NOTE: It is very important that you use 'sherlock_spinner_item' here
         //      and NOT 'simple_spinner_item' or you will see text color problems
         ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(this, R.array.locations, R.layout.sherlock_spinner_item);
         list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
         getSupportActionBar().setListNavigationCallbacks(list, this);
     }
 
     @Override
     public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-        ((TextView)findViewById(R.id.text)).setText("Selected: " + itemPosition);
+        mSelected.setText("Selected: " + mLocations[itemPosition]);
         return true;
     }
 }
