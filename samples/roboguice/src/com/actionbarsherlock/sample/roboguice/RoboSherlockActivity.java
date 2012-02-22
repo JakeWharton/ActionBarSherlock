a9c32738397f6e8e8256d782a7b6bd88d052cb13
Jake Wharton <jakewharton@gmail.com>
2012-02-22 01:38:31 -0500

    http://youtu.be/p-3e0EkvIEM

--- samples/roboguice/src/com/actionbarsherlock/sample/roboguice/RoboSherlockActivity.java
+++ samples/roboguice/src/com/actionbarsherlock/sample/roboguice/RoboSherlockActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Michael Burton
+ * Copyright 2012 Jake Wharton
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
