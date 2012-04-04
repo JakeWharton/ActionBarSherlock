3ab19306bf488f91ef9740ad392121daeb890c63
nacitar sevaht <nacitar@ubercpp.com>
2012-04-04 15:37:54 -0500

    Adding missing [set/get]DividerPadding methods.

--- library/src/com/actionbarsherlock/internal/widget/IcsLinearLayout.java
+++ library/src/com/actionbarsherlock/internal/widget/IcsLinearLayout.java
@@ -107,6 +107,21 @@ public class IcsLinearLayout extends NineLinearLayout {
     }
 
     /**
+     * Set padding displayed on both ends of dividers.
+     * @param padding Padding value in pixels that will be applied to each end.
+     */
+    public void setDividerPadding (int padding) {
+        mDividerPadding=padding;
+    }
+
+    /**
+     * Get the padding size used to inset dividers in pixels.
+     */
+    public int getDividerPadding() {
+        return mDividerPadding;
+    }
+
+    /**
      * Get the width of the current divider drawable.
      *
      * @hide Used internally by framework.
