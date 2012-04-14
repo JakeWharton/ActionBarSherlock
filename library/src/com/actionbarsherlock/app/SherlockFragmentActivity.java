c5201a7d5b29112f71b7d2133867a6cad6acac81
Jake Wharton <jw@squareup.com>
2012-04-14 02:24:18 -0700

    Unused import.

--- library/src/com/actionbarsherlock/app/SherlockFragmentActivity.java
+++ library/src/com/actionbarsherlock/app/SherlockFragmentActivity.java
@@ -1,7 +1,6 @@
 package com.actionbarsherlock.app;
 
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
