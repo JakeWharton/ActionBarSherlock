a77ea94b685f8685f677e63979453599164150e3
Michael Yin <michael@turntable.fm>
2012-10-15 22:41:59 -0400

    add example of known bug for issue #659 searchview does not display suggestions

---
+++ samples/known-bugs/src/com/actionbarsherlock/sample/knownbugs/Issue659SuggestionsProvider.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.actionbarsherlock.sample.knownbugs;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+/**
+ * Sample search recent suggestions provider. For Issue 659
+ * @author michael@turntable.fm
+ *
+ */
+public class Issue659SuggestionsProvider extends SearchRecentSuggestionsProvider {
+    public final static String AUTHORITY = "com.actionbarsherlock.sample.knownbugs.Issue659SuggestionsProvider";
+    public final static int MODE = DATABASE_MODE_QUERIES;
+
+    public Issue659SuggestionsProvider() {
+        setupSuggestions(AUTHORITY, MODE);
+    }
+}
