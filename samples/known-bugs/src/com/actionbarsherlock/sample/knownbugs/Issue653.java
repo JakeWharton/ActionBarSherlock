00706029b820950ca13d4b7f39eb041a77f65b46
lity <exceeding@gmail.com>
2013-01-21 14:28:07 +0800

    add test case to reproduce issue #653

---
+++ samples/known-bugs/src/com/actionbarsherlock/sample/knownbugs/Issue653.java
@@ -0,0 +1,51 @@
+package com.actionbarsherlock.sample.knownbugs;
+
+import android.annotation.TargetApi;
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+
+/*
+ * search suggestion can be fired if import android.widget.SearchView
+ * search suggestion can not be fired if com.actionbarsherlock.widget.SearchView
+ */
+
+//import com.actionbarsherlock.widget.SearchView;
+import android.widget.SearchView;
+
+
+
+public class Issue653 extends SherlockActivity  {
+	SearchManager searchManager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+    	setTheme(R.style.Theme_Sherlock);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.issue653);
+		
+		searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+        SearchView searchView = new SearchView(getSupportActionBar().getThemedContext());
+		searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        
+        menu.add("Search")
+        .setActionView(searchView)
+        .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		
+		return true;
+	}
+}
