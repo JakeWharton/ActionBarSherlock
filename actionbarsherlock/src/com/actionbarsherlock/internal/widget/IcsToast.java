35e2253c8ceb7dc8f9d0d97355491fdf0b34db89
Prototik <prototypegamez@gmail.com>
2013-01-22 15:34:27 +0800

    Fix marks by Jake

--- actionbarsherlock/src/com/actionbarsherlock/internal/widget/IcsToast.java
+++ actionbarsherlock/src/com/actionbarsherlock/internal/widget/IcsToast.java
@@ -17,14 +17,17 @@ public class IcsToast extends Toast {
     private static final String TAG = "Toast";
 
     public static Toast makeText(Context context, CharSequence s, int duration) {
-        if(VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB) {
+        if (VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH) {
             return Toast.makeText(context, s, duration);
         }
         IcsToast toast = new IcsToast(context);
         toast.setDuration(duration);
         TextView view = new TextView(context);
         view.setText(s);
-        view.setTextColor(0xFFDADADA);
+        // Original AOSP using reference on @android:color/bright_foreground_dark
+        // bright_foreground_dark - reference on @android:color/background_light
+        // background_light - 0xffffffff
+        view.setTextColor(0xffffffff);
         view.setGravity(Gravity.CENTER);
         view.setBackgroundResource(R.drawable.abs__toast_frame);
         toast.setView(view);
@@ -41,7 +44,7 @@ public class IcsToast extends Toast {
 
     @Override
     public void setText(CharSequence s) {
-        if(VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB) {
+        if (VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH) {
             super.setText(s);
             return;
         }
@@ -51,7 +54,7 @@ public class IcsToast extends Toast {
         try {
             ((TextView) getView()).setText(s);
         } catch (ClassCastException e) {
-            Log.e(IcsToast.TAG, "This Toast was not created with IcsToast.makeText", e);
+            Log.e(TAG, "This Toast was not created with IcsToast.makeText", e);
         }
     }
 }
