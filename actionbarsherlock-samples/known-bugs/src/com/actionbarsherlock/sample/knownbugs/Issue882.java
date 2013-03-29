199557652ccc6ed903b7cd05056b6b92c1b88b69
Philip Schiffer <philip.schiffer@gmail.com>
2013-03-29 19:51:35 +0100

    Update Issue882.java
    replaced tabs with 4 spaces

--- actionbarsherlock-samples/known-bugs/src/com/actionbarsherlock/sample/knownbugs/Issue882.java
+++ actionbarsherlock-samples/known-bugs/src/com/actionbarsherlock/sample/knownbugs/Issue882.java
@@ -21,111 +21,111 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 public class Issue882 extends SherlockFragmentActivity implements
-		OnPageChangeListener, TabListener {
-	private static final int COUNT = 20;
+        OnPageChangeListener, TabListener {
+    private static final int COUNT = 20;
 
-	ViewPager mPager;
+    ViewPager mPager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.issue882);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.issue882);
 
-		mPager = (ViewPager) findViewById(R.id.pager);
-		mPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
-		mPager.setOnPageChangeListener(this);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        mPager.setOnPageChangeListener(this);
 
-		ActionBar ab = getSupportActionBar();
-		ab.setNavigationMode(NAVIGATION_MODE_TABS);
-		for (int i = 1; i <= COUNT; i++) {
-			ab.addTab(ab.newTab().setText("Fragment " + i).setTabListener(this));
-		}
-	}
+        ActionBar ab = getSupportActionBar();
+        ab.setNavigationMode(NAVIGATION_MODE_TABS);
+        for (int i = 1; i <= COUNT; i++) {
+            ab.addTab(ab.newTab().setText("Fragment " + i).setTabListener(this));
+        }
+    }
 
-	@Override
-	public void onPageScrolled(int position, float positionOffset,
-			int positionOffsetPixels) {
-	}
+    @Override
+    public void onPageScrolled(int position, float positionOffset,
+            int positionOffsetPixels) {
+    }
 
-	@Override
-	public void onPageSelected(int position) {
-		getSupportActionBar().setSelectedNavigationItem(position);
-	}
+    @Override
+    public void onPageSelected(int position) {
+        getSupportActionBar().setSelectedNavigationItem(position);
+    }
 
-	@Override
-	public void onPageScrollStateChanged(int state) {
-	}
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
 
-	@Override
-	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-		mPager.setCurrentItem(tab.getPosition());
-	}
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mPager.setCurrentItem(tab.getPosition());
+    }
 
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-	}
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
 
-	@Override
-	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-	}
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
 
-	public static class MyAdapter extends FragmentStatePagerAdapter {
-		public MyAdapter(FragmentManager fm) {
-			super(fm);
-		}
+    public static class MyAdapter extends FragmentStatePagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-		@Override
-		public int getCount() {
-			return COUNT;
-		}
+        @Override
+        public int getCount() {
+            return COUNT;
+        }
 
-		@Override
-		public Fragment getItem(int position) {
-			return BoringFragment.newInstance(position + 1);
-		}
-	}
+        @Override
+        public Fragment getItem(int position) {
+            return BoringFragment.newInstance(position + 1);
+        }
+    }
 
-	public static class BoringFragment extends SherlockFragment {
-		int mNum;
+    public static class BoringFragment extends SherlockFragment {
+        int mNum;
 
-		/**
-		 * Create a new instance of CountingFragment, providing "num" as an
-		 * argument.
-		 */
-		static BoringFragment newInstance(int num) {
-			BoringFragment f = new BoringFragment();
+        /**
+         * Create a new instance of CountingFragment, providing "num" as an
+         * argument.
+         */
+        static BoringFragment newInstance(int num) {
+            BoringFragment f = new BoringFragment();
 
-			// Supply num input as an argument.
-			Bundle args = new Bundle();
-			args.putInt("num", num);
-			f.setArguments(args);
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            f.setArguments(args);
 
-			return f;
-		}
+            return f;
+        }
 
-		/**
-		 * When creating, retrieve this instance's number from its arguments.
-		 */
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			mNum = getArguments() != null ? getArguments().getInt("num") : 1;
-		}
+        /**
+         * When creating, retrieve this instance's number from its arguments.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+        }
 
-		/**
-		 * The Fragment's UI is just a simple text view showing its instance
-		 * number.
-		 */
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			TextView tv = new TextView(getActivity());
-			tv.setLayoutParams(new ViewGroup.LayoutParams(MATCH_PARENT,
-					MATCH_PARENT));
-			tv.setText("Fragment #" + mNum);
-			tv.setGravity(CENTER);
-			return tv;
-		}
+        /**
+         * The Fragment's UI is just a simple text view showing its instance
+         * number.
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            TextView tv = new TextView(getActivity());
+            tv.setLayoutParams(new ViewGroup.LayoutParams(MATCH_PARENT,
+                    MATCH_PARENT));
+            tv.setText("Fragment #" + mNum);
+            tv.setGravity(CENTER);
+            return tv;
+        }
 
-	}
+    }
 }
