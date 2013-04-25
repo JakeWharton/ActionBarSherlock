f7d1b5ff715797cc768b4b0294e0bc1673fac4a6
gomino <abezzarg@gmail.com>
2013-04-25 15:52:08 +0200

    fix NullPointerException when trying to select invalid tab position

--- library/src/com/actionbarsherlock/internal/widget/ScrollingTabContainerView.java
+++ library/src/com/actionbarsherlock/internal/widget/ScrollingTabContainerView.java
@@ -241,17 +241,19 @@ public class ScrollingTabContainerView extends NineHorizontalScrollView
 
     public void animateToTab(final int position) {
         final View tabView = mTabLayout.getChildAt(position);
-        if (mTabSelector != null) {
-            removeCallbacks(mTabSelector);
+        if (tabView!=null){
+        	if (mTabSelector != null) {
+        		removeCallbacks(mTabSelector);
+        	}
+        	mTabSelector = new Runnable() {
+        		public void run() {
+        			final int scrollPos = tabView.getLeft() - (getWidth() - tabView.getWidth()) / 2;
+        			smoothScrollTo(scrollPos, 0);
+        			mTabSelector = null;
+        		}
+        	};
+        	post(mTabSelector);
         }
-        mTabSelector = new Runnable() {
-            public void run() {
-                final int scrollPos = tabView.getLeft() - (getWidth() - tabView.getWidth()) / 2;
-                smoothScrollTo(scrollPos, 0);
-                mTabSelector = null;
-            }
-        };
-        post(mTabSelector);
     }
 
     @Override
